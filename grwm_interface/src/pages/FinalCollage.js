a5086cb7308bc968f9062bd49f4e001001d93830
ChinikaC <clcharles@hotmail.co.uk>
2023-04-05 11:26:37 +0100

    Display button functionality complete

--- grwm_interface/src/pages/FinalCollage.js
+++ grwm_interface/src/pages/FinalCollage.js
@@ -4,9 +4,12 @@ import Collage from "../components/Collage";
 
   const FinalCollage = ({ collages, submitPreferences, postCollageToUser}) => {
 
+    const [displayButton, setDisplayButton] = useState(true);
     const [chosenIndex, setChosenIndex] = useState(0);
+    const [showButton, setShowButton] = useState(true);
+    
     const nextIndex = ()=> {
-      if(chosenIndex + 1 < collages.length - 1) {
+      if(chosenIndex + 1 < collages.length) {
         setChosenIndex(chosenIndex+1);
       }
     }
@@ -17,6 +20,7 @@ import Collage from "../components/Collage";
       nextIndex();
   };
 
+
     console.log(collages);
     console.log(submitPreferences);
 
@@ -26,8 +30,8 @@ import Collage from "../components/Collage";
           <h2>Thank You for Filling Out The Style Quiz!</h2>
         <h3>The Outfit Designs We Believe Best Suits Your Style Are:</h3>
           <div className={`category ${submitPreferences}`}>
-               {collages && collages.length > 0 && collages[chosenIndex].name ? <Collage deleteCollage={nextIndex} 
-               handleAddToCollageList={handleAddToCollageList} collage={collages[chosenIndex]}/> : <></>}
+             {collages && collages.length > 0 && collages[chosenIndex].name ? <Collage deleteCollage={nextIndex} 
+               handleAddToCollageList={handleAddToCollageList} collage={collages[chosenIndex]} displayButtons={true}/> : <></> }
           </div>
           </div>
        );
